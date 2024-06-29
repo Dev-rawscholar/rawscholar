0ac016fe652326b5313af925398727bff16c1f40
RameezRizvan <rameezrizvan04@gmail.com>
2024-06-29 10:38:30 +0530

    new commits

--- frontend/src/Pages/Countries/Countries.jsx
+++ frontend/src/Pages/Countries/Countries.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import germany from "../../assets/flags/germany.png";
 import {  useNavigate } from "react-router-dom";
@@ -49,3 +50,56 @@ function Countries({ setShow }) {
 }
 
 export default Countries;
+=======
+
+import germany from "../../assets/flags/germany.png";
+import {  useNavigate } from "react-router-dom";
+import {useFrappeGetDocList} from 'frappe-react-sdk'
+import { useContext, useEffect } from "react";
+import { countryContext } from "../../Components/ContextShare";
+
+function Countries({ setShow }) {
+   useEffect(()=>{
+      setShow(true);
+   })
+
+   const {data,error} = useFrappeGetDocList('Countries')
+   console.log(error);
+   const { setCountryData } = useContext(countryContext)
+   const navigate = useNavigate()
+   const goToCountry = (country) =>{
+      setCountryData(country)
+      navigate("/universities")
+   }
+
+   return (
+      <section id="countriesSection" className="container">
+         <div className="countriesContainer shapeParent">
+            <div className="my-5">
+               <div className="shape"></div>
+               <h3 className="ms-4">Countries</h3>
+            </div>
+            <div className="d-flex flex-wrap justify-content-center align-items-center column-gap-3 my-5">
+                  {data?.map((country,i)=>(
+                     <div onClick={()=>goToCountry(country)} style={{ textDecoration: "none" }} key={i}>
+                           <div>
+                           <img
+                              src={germany}
+                              height={180}
+                              alt="Country pic"
+                              className="rounded border"
+                           />
+                           <div className="title text-center">
+                              <p>{country.name}</p>
+                           </div>
+                        </div>
+                     </div>
+                  ))}
+            </div>
+         </div>
+      </section>
+   );
+}
+
+export default Countries;
+>>>>>>> 692629f (new commit)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,18 @@ import ContextShare from "./Components/ContextShare.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+<<<<<<< HEAD
     <ContextShare>
       <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
         <App />
       </BrowserRouter>
     </ContextShare>
+=======
+   <ContextShare>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+   </ContextShare>
+>>>>>>> 692629f (new commit)
   </React.StrictMode>
 );
