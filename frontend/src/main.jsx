ab041028067b1ae27da7da6fb17ca8e7f11fcb92
RameezRizvan <rameezrizvan04@gmail.com>
2024-06-29 10:57:14 +0530

    fix:"removed head"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,18 +7,15 @@ import ContextShare from "./Components/ContextShare.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <ContextShare>
       <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
         <App />
       </BrowserRouter>
     </ContextShare>
-=======
    <ContextShare>
       <BrowserRouter>
         <App />
       </BrowserRouter>
    </ContextShare>
->>>>>>> 692629f (new commit)
   </React.StrictMode>
 );
