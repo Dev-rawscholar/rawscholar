05913dee24a7f3d814bf0b100244d88875020869
RameezRizvan <rameezrizvan04@gmail.com>
2024-07-05 16:22:31 +0530

    feat:"new roles,numbercards added"

--- rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
+++ rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
@@ -1,9 +1,5 @@
 (() => {
-<<<<<<<< HEAD:rawscholar/public/dist/js/rawscholar.bundle.JZMVBD5S.js
-  // frappe-html:/home/goks/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_notes.html
-========
   // frappe-html:/home/rameez-rizvan/raw/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_notes.html
->>>>>>>> 1651ae9 (fix:"chart deleted"):rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
   frappe.templates["student_notes"] = `<div class="notes-section col-xs-12">
 	<div class="new-btn pb-3">
 		<button class="btn btn-sm small new-note-btn mr-1">
@@ -79,11 +75,7 @@
 
 </style>`;
 
-<<<<<<<< HEAD:rawscholar/public/dist/js/rawscholar.bundle.JZMVBD5S.js
-  // frappe-html:/home/goks/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_activities.html
-========
   // frappe-html:/home/rameez-rizvan/raw/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_activities.html
->>>>>>>> 1651ae9 (fix:"chart deleted"):rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
   frappe.templates["student_activities"] = `<div class="open-activities">
 	<div class="new-btn pb-3">
 		<span>
@@ -261,11 +253,7 @@
 }
 </style>`;
 
-<<<<<<<< HEAD:rawscholar/public/dist/js/rawscholar.bundle.JZMVBD5S.js
-  // frappe-html:/home/goks/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_qualification.html
-========
   // frappe-html:/home/rameez-rizvan/raw/frappe-bench/apps/rawscholar/rawscholar/public/js/templates/student_qualification.html
->>>>>>>> 1651ae9 (fix:"chart deleted"):rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
   frappe.templates["student_qualification"] = `<div class="qualification-section col-xs-12">
 	<div class="all-qualification">
         {% if (qualifications.length) { %}
@@ -374,8 +362,4 @@
 
 </style>`;
 })();
-<<<<<<<< HEAD:rawscholar/public/dist/js/rawscholar.bundle.JZMVBD5S.js
-//# sourceMappingURL=rawscholar.bundle.JZMVBD5S.js.map
-========
 //# sourceMappingURL=rawscholar.bundle.WLWGV5Y6.js.map
->>>>>>>> 1651ae9 (fix:"chart deleted"):rawscholar/public/dist/js/rawscholar.bundle.WLWGV5Y6.js
