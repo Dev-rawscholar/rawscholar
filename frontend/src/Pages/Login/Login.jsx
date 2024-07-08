a4628231539ecc16eeb19bd9d8268d58425ba0c7
Dev-rawscholar <rawscholar.dev@gmail.com>
2024-07-08 18:22:14 +0530

    Update Login.jsx

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -67,7 +67,6 @@ function Login({ setShow }) {
           <input
             className="inputBox"
             type="email"
-            type="email"
             name="email"
             placeholder="Enter Email"
             onChange={(e) => getInputData(e)}
